207c9d1a28d9d24f4b43a5ad6b2503f771ad09a9
redew97 <40416476+redew97@users.noreply.github.com>
2018-07-05 22:20:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ var HTTPS = require('https');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex =  /bro/i;
+ // var request = JSON.parse(this.req.chunks[0]),
+  var botRegex =  /bro/i;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
